8bbd45686c46a4c59de163517d7f1411cec93985
gnambiar63 <gokul.nambiar@spit.ac.in>
2019-10-09 23:41:15 +0530

    backend of login and signup

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,27 @@
-const express =  require('express');
 
+
+const http = require("http");
+
+const hostname = '127.0.0.1';
+
+const port = 3000;
+
+const server = http.createServer(function(req, res) {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+/*http.createServer(function (request, response) {
+    // Send the HTTP header 
+    // HTTP Status: 200 : OK
+    // Content Type: text/plain
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    
+    // Send the response body as "Hello World"
+    response.end('Hello World\n');
+ }).listen(8081);*/
+
+server.listen(port, hostname, function() {
+  console.log('Server running at http://'+ hostname + ':' + port + '/');
+});
